ddfa65cfb9b5156dff4bdf48dd121bfd37867fb2
Raposo <92121267+at-raposo@users.noreply.github.com>
2023-03-02 16:45:21 -0300

    Create class 2.c
    Esse programa mostra que o LED vai ficar acesso. Esse LED que vai se acender é um LED padrão que fica conectado a porta logica 13 do Arduino

---
+++ First classes/class 2.c
@@ -0,0 +1,14 @@
+void setup() {
+  pinMode(13,OUTPUT); // Aqui estamos selecionado a porta 13 para a saida
+  // INPUT = Entrada / OUTPUT = Saida
+  // definimos o modo da porta logica
+  
+
+}
+
+void loop() {
+  digitalWrite(13,HIGH); //HIGH = Porta ligada(fornece 5v) - LOW = porta desligada(fornece 0v)
+  // aqui iremos definir o estado da porta
+  
+
+}
